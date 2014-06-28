6a71d980b35712a9245d2e6981cfdf81dfe6b790
marcojrzx <marcojrzx@hotmail.com>
2014-06-27 22:10:48 -0500

    funciona el return self.nombre en models

--- sicla/logica/models.py
+++ sicla/logica/models.py
@@ -1,14 +1,11 @@
 #enconding: utf-8
 from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
 
 class Autor(models.Model):
-	nombre = models.TextField(max_length=45)
-	sexo = models.CharField(max_length=2) 
-
-def __unicode__(self):
+ nombre = models.TextField(max_length=45)
+ sexo = models.CharField(max_length=2) 
+ 
+ def __unicode__(self):
    return self.nombre	
 
 class Articulo(models.Model):
@@ -16,4 +13,4 @@ class Articulo(models.Model):
   articulo = models.TextField(max_length=10000)
 
 def __unicode__(self):
-     return self.articulo
+   return self.articulo
